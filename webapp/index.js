bf9b4da51f818947370df955708be8eeb57a2d7f
Eduardo A Oliveira <eduardoallves@hotmail.com>
2021-12-21 16:46:14 -0300

    Step 5: One-Way Data Binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,28 +1,29 @@
-sap.ui.require(
-  ["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/XMLView"],
-  function (JSONModel, XMLView) {
-    "use strict";
+sap.ui.require([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/model/BindingMode"
+], function (JSONModel, XMLView, BindingMode) {
+	"use strict";
 
-    // Attach an anonymous function to the SAPUI5 'init' event
-    sap.ui.getCore().attachInit(function () {
-      // Create a JSON model from an object literal
-      var oModel = new JSONModel({
-        firstName: "Harry",
-        lastName: "Hawk",
-        enabled: true,
-        panelHeaderText: "Data Binding Basics",
-      });
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+		// Create a JSON model from an object literal
+		var oModel = new JSONModel({
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			panelHeaderText: "Data Binding Basics"
+		});
 
-      // Assign the model object to the SAPUI5 core
-      sap.ui.getCore().setModel(oModel);
+		oModel.setDefaultBindingMode(BindingMode.OneWay);
 
-      // Display a text element whose text is derived
-      // from the model object
 
-      // Display the XML view called "App"
-      new XMLView({
-        viewName: "sap.ui.demo.data-binding.view.App",
-      }).placeAt("content");
-    });
-  }
-);
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
+
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.data-binding.view.App"
+		}).placeAt("content");
+	});
+});
