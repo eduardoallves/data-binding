6d0e307597144defe749fa01da53b449bd613628
Eduardo A Oliveira <eduardoallves@hotmail.com>
2021-12-21 17:27:56 -0300

    Step 11: Validation Using the Message Manager

--- webapp/index.js
+++ webapp/index.js
@@ -1,41 +1,48 @@
-sap.ui.require([
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/mvc/XMLView",
-	"sap/ui/model/resource/ResourceModel"
-], function (JSONModel, XMLView, ResourceModel) {
-	"use strict";
+sap.ui.require(
+  [
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (JSONModel, XMLView, ResourceModel) {
+    "use strict";
 
-	// Attach an anonymous function to the SAPUI5 'init' event
-	sap.ui.getCore().attachInit(function () {
-		var oModel = new JSONModel({
-			firstName: "Harry",
-			lastName: "Hawk",
-			enabled: true,
-			address: {
-				street: "Dietmar-Hopp-Allee 16",
-				city: "Walldorf",
-				zip: "69190",
-				country: "Germany"
-			},
-			salesAmount: 12345.6789,
-			currencyCode: "EUR"
+    // Attach an anonymous function to the SAPUI5 'init' event
+    sap.ui.getCore().attachInit(function () {
+      var oModel = new JSONModel({
+        firstName: "Harry",
+        lastName: "Hawk",
+        enabled: true,
+        address: {
+          street: "Dietmar-Hopp-Allee 16",
+          city: "Walldorf",
+          zip: "69190",
+          country: "Germany",
+        },
+        salesAmount: 12345.6789,
+        currencyCode: "EUR",
+      });
 
-		});
+      // Assign the model object to the SAPUI5 core
+      sap.ui.getCore().setModel(oModel);
 
-		// Assign the model object to the SAPUI5 core
-		sap.ui.getCore().setModel(oModel);
+      var oResourceBundle = new ResourceModel({
+        bundleName: "sap.ui.demo.data-binding.i18n.i18n",
+        supportedLocales: ["", "de"],
+        fallbackLocale: "",
+      });
 
-		var oResourceBundle = new ResourceModel({
-			bundleName: "sap.ui.demo.data-binding.i18n.i18n",
-			supportedLocales: ["", "de"],
-			fallbackLocale: ""
-		});
+      sap.ui.getCore().setModel(oResourceBundle, "i18n");
 
-		sap.ui.getCore().setModel(oResourceBundle, "i18n");
+      // Display the XML view called "App"
+      var oView = new XMLView({
+        viewName: "sap.ui.demo.data-binding.view.App",
+      });
 
-		// Display the XML view called "App"
-		new XMLView({
-			viewName: "sap.ui.demo.data-binding.view.App"
-		}).placeAt("content");
-	});
-});
+      // Register the view with the message manager
+      sap.ui.getCore().getMessageManager().registerObject(oView, true);
+
+      oView.placeAt("content");
+    });
+  }
+);
