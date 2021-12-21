dafb0c8348f51526d4aad0e734a7697edb65fba1
Eduardo A Oliveira <eduardoallves@hotmail.com>
2021-12-21 17:24:57 -0300

    Step 10: Property Formatting Using Data Types

--- webapp/index.js
+++ webapp/index.js
@@ -1,41 +1,41 @@
-sap.ui.require(
-  [
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/core/mvc/XMLView",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  function (JSONModel, XMLView, ResourceModel) {
-    "use strict";
+sap.ui.require([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
+	"use strict";
 
-    // Attach an anonymous function to the SAPUI5 'init' event
-    sap.ui.getCore().attachInit(function () {
-      var oModel = new JSONModel({
-        firstName: "Harry",
-        lastName: "Hawk",
-        enabled: true,
-        address: {
-          street: "Dietmar-Hopp-Allee 16",
-          city: "Walldorf",
-          zip: "69190",
-          country: "Germany",
-        },
-      });
+	// Attach an anonymous function to the SAPUI5 'init' event
+	sap.ui.getCore().attachInit(function () {
+		var oModel = new JSONModel({
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			address: {
+				street: "Dietmar-Hopp-Allee 16",
+				city: "Walldorf",
+				zip: "69190",
+				country: "Germany"
+			},
+			salesAmount: 12345.6789,
+			currencyCode: "EUR"
 
-      // Assign the model object to the SAPUI5 core
-      sap.ui.getCore().setModel(oModel);
+		});
 
-      var oResourceModel = new ResourceModel({
-        bundleName: "sap.ui.demo.data-binding.i18n.i18n",
-        supportedLocales: ["", "de"],
-        fallbackLocale: "",
-      });
+		// Assign the model object to the SAPUI5 core
+		sap.ui.getCore().setModel(oModel);
 
-      sap.ui.getCore().setModel(oResourceModel, "i18n");
+		var oResourceBundle = new ResourceModel({
+			bundleName: "sap.ui.demo.data-binding.i18n.i18n",
+			supportedLocales: ["", "de"],
+			fallbackLocale: ""
+		});
 
-      // Display the XML view called "App"
-      new XMLView({
-        viewName: "sap.ui.demo.data-binding.view.App",
-      }).placeAt("content");
-    });
-  }
-);
+		sap.ui.getCore().setModel(oResourceBundle, "i18n");
+
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.data-binding.view.App"
+		}).placeAt("content");
+	});
+});
